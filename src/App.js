76221c60bf07a301783facd78603a58918c4e604
mpjaramillo <mapujaramillo8@gmail.com>
2020-02-19 08:38:57 -0500

    modificaciones CSS

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (<Router>
     <div className="App">
       <header className="App-header">
-        <Navbar bg="light" variant="dark">
+        <Navbar bg="dark" variant="dark">
           <Container>
 
             <Navbar.Brand>
